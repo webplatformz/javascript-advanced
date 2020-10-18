1949b8205f732c27ac70004beebe259a1f993b1c
Stefan Reichhart <stefan.reichhart@zuehlke.com>
2020-10-18 17:24:12 +0200

    stre; spelling, pure functions exercise 2 fixed

--- exercises/src/solutions/02-from-an-impure-to-a-pure-function/exercise2.solution.spec.js
+++ exercises/src/solutions/02-from-an-impure-to-a-pure-function/exercise2.solution.spec.js
@@ -1,16 +1,18 @@
 describe("method referencing external state", () => {
 
-  it("is pure", () => {
+    it("is pure", () => {
 
-    // solution: remove the reference and inject the initial value directly
-    function count(counter) {
-      while (counter < 5) {
-        counter += 1;
-      }
-    }
-    
-    count(5);
+        // solution: remove the reference and inject the initial value directly
+        function count(iterations) {
+            let counter = iterations;
+            while (counter < 5) {
+                counter += 1;
+            }
+            return counter;
+        }
 
-  });
+        count(5);
+
+    });
 
 });
